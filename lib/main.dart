5bcc0488f91245ae2f5d06f11ffeec09dbe01547
Gil Medeiros <52362659+GilMedeiros@users.noreply.github.com>
2020-02-18 02:01:44 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:parse_login_tuto/home.dart';
 import 'package:parse_server_sdk/parse_server_sdk.dart';
 
 //Keys from back4app
-const String PARSE_APP_ID = 'PDRmzMpVpkpwQ9oCOrlevTEg5lZ6i9Y4x7WGdJJy';
+const String PARSE_APP_ID = 'YOUR_ID';
 
 const String PARSE_APP_URL = 'https://parseapi.back4app.com';
 
-const String MASTER_KEY = 'Kp45FC6SVuP85XSZFmLBntOEEiQyrfHnOx8ISAu1';
+const String MASTER_KEY = 'YOUR_KEY';
 
 
 Future<void> main() async {
